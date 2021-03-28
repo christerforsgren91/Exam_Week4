a87b18fd7ea16e8ff10e4496c2bfc30deb2a580c
Christer <christerforsgren91@gmail.com>
2021-03-28 14:06:05 +0200

    added a lib folder and a rubyfile with answers

---
+++ lib/rubyanswers.rb
@@ -0,0 +1,24 @@
+x = 'virable'
+## data typ is a string
+
+## and some other data types
+age = 29 ## Integer
+job = 'student' ## String
+weight = 80.03 ## Float
+nerd = true ## Boolean
+friuts = [] ## Array
+food = {:ke => 'kebab'} ## Hash, a symbol and a string.
+
+cars = ['Audi', 'Ford']
+cars[0] = {weels: 4, max_speed: '240kph', color: 'white'}
+
+## its wrong, couldnt solve it.
+
+
+
+
+
+
+
+
+
